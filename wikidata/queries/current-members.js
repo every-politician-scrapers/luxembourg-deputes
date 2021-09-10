8787358c674726c38f03f9e1664aaab6b41ff567
Tony Bowden <tony@tmtm.com>
2021-09-10 20:01:30 +0300

    Improve SPARQL

--- wikidata/queries/current-members.js
+++ wikidata/queries/current-members.js
@@ -3,28 +3,28 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
-  return `SELECT ?item ?name ?party (COALESCE(?partyShort, ?party) AS ?group) ?district ?constituency ?start
+  return `SELECT ?item ?name ?party ?group
          (STRAFTER(STR(?statement), '/statement/') AS ?psid)
     WHERE
     {
       ?item p:P39 ?statement .
       ?statement ps:P39 wd:${meta.legislature.member} ; pq:P2937 wd:${meta.legislature.term.id} .
-      OPTIONAL { ?statement pq:P580 ?start }
       FILTER NOT EXISTS { ?statement pq:P582 ?end }
-      OPTIONAL { ?statement pq:P768 ?district }
-      OPTIONAL { ?statement pq:P4100 ?party }
+
       OPTIONAL {
         ?item wdt:P102 ?party .
-        OPTIONAL { ?party wdt:P1813 ?partyShort FILTER (LANG(?partyShort) = 'lb')}
-        OPTIONAL { ?party rdfs:label ?partyName FILTER (LANG(?partyName) = 'lb')}
+        OPTIONAL { ?party wdt:P1813 ?partyShort FILTER (LANG(?partyShort) = '${meta.source.lang.code}')}
+        OPTIONAL { ?party rdfs:label ?partyName FILTER (LANG(?partyName) = '${meta.source.lang.code}')}
         BIND(COALESCE(?partyShort, ?partyName) as ?group)
       }
 
-      SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "lb,fr,en". 
-        ?item rdfs:label ?name .
-        ?district rdfs:label ?constituency .
+      OPTIONAL {
+        ?statement prov:wasDerivedFrom ?ref .
+        ?ref pr:P854 ?source FILTER CONTAINS(STR(?source), '${meta.source.url}')
+        OPTIONAL { ?ref pr:P1810 ?sourceName }
       }
+      OPTIONAL { ?item rdfs:label ?wdLabel FILTER(LANG(?wdLabel) = "${meta.source.lang.code}") }
+      BIND(COALESCE(?sourceName, ?wdLabel) AS ?name)
     }
     ORDER BY ?item`
 }
