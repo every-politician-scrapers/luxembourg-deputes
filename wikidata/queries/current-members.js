66939dea9b66ed34a294968d8222705224d5457e
Tony Bowden <tony@tmtm.com>
2021-09-10 17:38:57 +0300

    Add party info directly from person

--- wikidata/queries/current-members.js
+++ wikidata/queries/current-members.js
@@ -3,7 +3,7 @@ let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
 module.exports = function () {
-  return `SELECT ?item ?name ?party ?group ?district ?constituency ?start
+  return `SELECT ?item ?name ?party (COALESCE(?partyShort, ?party) AS ?group) ?district ?constituency ?start
          (STRAFTER(STR(?statement), '/statement/') AS ?psid)
     WHERE
     {
@@ -13,10 +13,16 @@ module.exports = function () {
       FILTER NOT EXISTS { ?statement pq:P582 ?end }
       OPTIONAL { ?statement pq:P768 ?district }
       OPTIONAL { ?statement pq:P4100 ?party }
+      OPTIONAL {
+        ?item wdt:P102 ?party .
+        OPTIONAL { ?party wdt:P1813 ?partyShort FILTER (LANG(?partyShort) = 'lb')}
+        OPTIONAL { ?party rdfs:label ?partyName FILTER (LANG(?partyName) = 'lb')}
+        BIND(COALESCE(?partyShort, ?partyName) as ?group)
+      }
+
       SERVICE wikibase:label {
-        bd:serviceParam wikibase:language "fr,en". 
+        bd:serviceParam wikibase:language "lb,fr,en". 
         ?item rdfs:label ?name .
-        ?party rdfs:label ?group .
         ?district rdfs:label ?constituency .
       }
     }
