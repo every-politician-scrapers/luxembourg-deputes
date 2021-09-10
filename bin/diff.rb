f2f928628b87be4ce1d3c7ec29f9909ce3cc5639
Tony Bowden <tony@tmtm.com>
2021-09-10 20:39:55 +0300

    refresh data

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,11 +3,20 @@
 
 require 'every_politician_scraper/comparison'
 
-# Remove government members
+# Remove government members and standardise parties
 class Comparison < EveryPoliticianScraper::Comparison
-  def external
-    super.delete_if { |row| row[:seat] >= 100 }
+  REMAP = {
+    'Déi Lénk' => 'DÉI LÉNK'
+  }
+
+  def wikidata_csv_options
+    { converters: [->(val, field) { REMAP.fetch(val, val) }] }
   end
+
+  def external
+    super.delete_if { |row| (row[:seat] >= 100) || (row[:seat] == 0) }
+  end
+
 end
 
 diff = Comparison.new('wikidata/results/current-members.csv', 'data/official.csv').diff
